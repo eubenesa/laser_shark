8355cd22e02d836f7d56126f983935928f166928
Eubene Sa <eubene.sa@gmail.com>
2014-08-07 13:13:08 -0700

    rails g migration for enabling hstore for postgres db, ran rake db:migrate after successfully

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720030348) do
+ActiveRecord::Schema.define(version: 20140807200352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "activities", force: true do |t|
     t.string   "name"
